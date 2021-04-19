4228970b3cfcb76d5489a39febc606afba919bc7
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-19 03:18:25 -0700

    Fixed the expected num of arguments, create an output file

--- decoder.cc
+++ decoder.cc
@@ -2,10 +2,10 @@
 #include <fstream>
 
 int main(int argc, char *argv[]){//take in number of command line arguments, and arguments
-	if (argc != 0){
-		return
+	if (argc != 2){
+		return 0;
 	}
-
+	ofstream File("outfile.txt"); //create the file - needs to be changed to have the correct name
 	while (true) {
 	  while (symbol < 0) {
 	    symbol = huff.decode(bitio.input_bit());
--- encoder.cc
+++ encoder.cc
@@ -2,5 +2,8 @@
 #include <fstream>
 
 int main(int argc, char *argv[]){//take in number of command line arguments, and arguments
+	if (argc != 0){
+		return 0;
+	}
 	return 1;
 }
