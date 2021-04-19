eb21514f9291e26d5dc0af1e9b58a947dad810c4
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-19 03:12:14 -0700

    include iostream and fstream, and added check for num of command line args

--- decoder.cc
+++ decoder.cc
@@ -1,4 +1,11 @@
-int main(){
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[]){//take in number of command line arguments, and arguments
+	if (argc != 0){
+		return
+	}
+
 	while (true) {
 	  while (symbol < 0) {
 	    symbol = huff.decode(bitio.input_bit());
--- encoder.cc
+++ encoder.cc
@@ -1,3 +1,6 @@
-int main(){
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[]){//take in number of command line arguments, and arguments
 	return 1;
 }
