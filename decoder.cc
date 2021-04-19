7e9e9d8b824f6df2242f20c807efea9f013820e0
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-19 10:20:22 -0700

    Use assertions instead of returns to check command line arguments

--- decoder.cc
+++ decoder.cc
@@ -4,9 +4,7 @@
 #include "bitio.hh"
 
 int main(int argc, char **argv){//take in number of command line arguments, and arguments
-	if (argc != 2){
-		return 0;
-	}
+	assert(argc == 2)
 	std::string filename; 
 	filename = argv[1]; //initialize and set filename to the name of the infile
 	std::ifstream Infile(filename); //open ifstream of infile
--- encoder.cc
+++ encoder.cc
@@ -3,25 +3,24 @@
 #include "huffman.hh"
 #include "bitio.hh"
 
-int main(int argc, char **argv){//take in number of command line arguments, and arguments
-	if (argc != 2){
-		return 0;
-	}
+int main(int argc, char **argv){//take in number of command line arguments, and the arguments
+
+	assert(argc == 2); //make sure there are two command line arguments
+
 	Huffman huff;
 	std::string filename; 
 	filename = argv[1]; //initialize and set filename to the name of the infile
 	std::ifstream Infile(filename);
 
+	filename.append(".comp"); //change the filename to have the appropriate extension
+	std::ofstream Outfile(filename); //open an outfile
+	Outfile<< "test";
 
 	// while (true) {
 	//     // symbol = huff.encode(bitio.input_bit());
 	// }
 
 	Infile.close(); //close the infile
-
-	filename.append(".comp"); //change the filename to have the appropriate extension
-	std::ofstream Outfile(filename); //open an outfile
-	Outfile<< "test";
 	Outfile.close(); //close the outfile
 
 	return 1;
