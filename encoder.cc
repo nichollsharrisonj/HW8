233c57ad5c317beaa22432c226e1f1ba60a05b09
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-19 03:28:16 -0700

    Close files, and take in the given command line argument to encode

--- encoder.cc
+++ encoder.cc
@@ -2,8 +2,19 @@
 #include <fstream>
 
 int main(int argc, char *argv[]){//take in number of command line arguments, and arguments
-	if (argc != 0){
+	if (argc != 2){
 		return 0;
 	}
+	string filename; 
+	filename = *argv[1];
+	ifstream File(filename);
+
+
+	// while (true) {
+	//     // symbol = huff.encode(bitio.input_bit());
+	// }
+	
+	File.close()
+
 	return 1;
 }
